6a2cad16711016c48c047a4610e47d99f3d896d2
Ivan Artemiev <artemiev@paive.com>
2022-06-21 00:18:12 +0300

    edit @ dev #3

--- test.sh
+++ test.sh
@@ -9,3 +9,19 @@ t=5
 cmd="sysctl -n vm.loadavg" # mac os
 # cmd="cat /proc/loadavg" # debian
 
+
+max=3
+
+i=0
+
+while [[ $i -lt $max ]]; do
+
+	res=`$cmd`
+	echo $res
+	sleep 5;
+
+	((i++))
+done
+
+echo "Exit after $max steps"
+
